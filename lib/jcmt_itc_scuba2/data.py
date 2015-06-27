0c79a67dd6696ba986bdfd79734ed5d27912544c
Graham Bell <g.bell@eaobservatory.org>
2015-06-26 14:02:55 -1000

    Add typical observation lengths for each mode
    These numbers were taken from the propscuba2itc.pl script.

--- lib/jcmt_itc_scuba2/data.py
+++ lib/jcmt_itc_scuba2/data.py
@@ -39,10 +39,11 @@ ITCParam = namedtuple(
 #     description:  description of the mode
 #     param_850:    timing parameters for 850um calculations
 #     param_450:    timing parameters for 450um calculations
+#     block_min:    typical obs. length (for overhead estimation) / minutes
 
 SCUBA2Mode = namedtuple(
     'SCUBA2Mode',
-    ('description', 'param_850', 'param_450'))
+    ('description', 'param_850', 'param_450', 'block_min'))
 
 # Information for each observing mode.
 
@@ -50,25 +51,30 @@ scuba2_modes = OrderedDict((
     ('daisy',
      SCUBA2Mode(description='Daisy: ~3 arcmin map',
                 param_850=ITCParam(tA=189, tB=-48, c=0.248312),
-                param_450=ITCParam(tA=689, tB=-118, c=0.062124))),
+                param_450=ITCParam(tA=689, tB=-118, c=0.062124),
+                block_min=30)),
 
     ('pong900',
      SCUBA2Mode(description='Pong 900: 15 arcmin map',
                 param_850=ITCParam(tA=407, tB=-104, c=0.053870),
-                param_450=ITCParam(tA=1483, tB=-254, c=0.013420))),
+                param_450=ITCParam(tA=1483, tB=-254, c=0.013420),
+                block_min=40)),
 
     ('pong1800',
      SCUBA2Mode(description='Pong 1800: 30 arcmin map',
                 param_850=ITCParam(tA=795, tB=-203, c=0.014113),
-                param_450=ITCParam(tA=2904, tB=-497, c=0.003500))),
+                param_450=ITCParam(tA=2904, tB=-497, c=0.003500),
+                block_min=40)),
 
     ('pong3600',
      SCUBA2Mode(description='Pong 3600: 1 degree map',
                 param_850=ITCParam(tA=1675, tB=-428, c=0.003180),
-                param_450=ITCParam(tA=6317, tB=-1082, c=0.000550))),
+                param_450=ITCParam(tA=6317, tB=-1082, c=0.000550),
+                block_min=40)),
 
     ('pong7200',
      SCUBA2Mode(description='Pong 7200: 2 degree map',
                 param_850=ITCParam(tA=3354, tB=-857, c=0.000794),
-                param_450=ITCParam(tA=12837, tB=-2200, c=0.00017919))),
+                param_450=ITCParam(tA=12837, tB=-2200, c=0.00017919),
+                block_min=40)),
 ))
