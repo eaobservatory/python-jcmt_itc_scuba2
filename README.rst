ec8956b328374e6c711fc30e66a545c706c7f175
Graham Bell <g.bell@eaobservatory.org>
2015-07-20 09:20:29 -1000

    Add packaging information

---
+++ README.rst
@@ -0,0 +1,26 @@
+SCUBA-2 Integration Time Calculator
+===================================
+
+Introduction
+------------
+
+This package contains the Python-based integration time calculator
+for SCUBA-2.  It is based on the original Perl
+`Astro::ITC::SCUBA2 <https://github.com/eaobservatory/perl-Astro-ITC-SCUBA2>`_
+module.
+
+License
+-------
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>
---
+++ setup.py
@@ -0,0 +1,34 @@
+# Copyright (C) 2015 East Asian Observatory
+# All Rights Reserved.
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 2 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful,but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc.,51 Franklin
+# Street, Fifth Floor, Boston, MA  02110-1301, USA
+
+from distutils.core import setup
+import sys
+
+sys.path.append('lib')
+from jcmt_itc_scuba2.version import version
+
+with open('README.rst') as f:
+    long_description = f.read()
+
+setup(
+    name='jcmt_itc_scuba2',
+    version=version,
+    description='SCUBA-2 Integration Time Calculator',
+    long_description=long_description,
+    url='https://github.com/eaobservatory/python-jcmt_itc_scuba2',
+    package_dir={'': 'lib'},
+    packages=['jcmt_itc_scuba2'])
